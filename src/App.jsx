14b6739850feab46b5b4bae579f576c865d8f243
Rafael Toledo Souza <ch1at@outlook.com>
2026-01-30 11:11:06 -0300

    creating hook for steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+
+import UserForm from "./components/UserForm";
+import ReviewForm from "./components/ReviewForm";
+import Thanks from "./components/Thanks";
+
+import { useForm } from "./hooks/UseForm";
+
 import "./App.css";
 
 function App() {
+  const formComponents = [<UserForm />, <ReviewForm />, <Thanks />];
+
+  const { currentStep, currentComponent } = useForm(formComponents);
+
   return (
     <div className="app">
       <div className="header">
@@ -14,7 +25,7 @@ function App() {
       <div className="form-container">
         <p>steps</p>
         <form>
-          <div className="inputs-container"></div>
+          <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
             <button type="button">
               <GrFormPrevious />
