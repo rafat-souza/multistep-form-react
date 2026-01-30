8680bd5220325acfe0dd845b90ee51fc4fa624c2
Rafael Toledo Souza <ch1at@outlook.com>
2026-01-30 15:32:09 -0300

    steps icons

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { FiSend } from "react-icons/fi";
 import UserForm from "./components/UserForm";
 import ReviewForm from "./components/ReviewForm";
 import Thanks from "./components/Thanks";
+import Steps from "./components/Steps";
 
 import { useForm } from "./hooks/UseForm";
 
@@ -25,7 +26,7 @@ function App() {
         </p>
       </div>
       <div className="form-container">
-        <p>steps</p>
+        <Steps currentStep={currentStep} />
         <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
