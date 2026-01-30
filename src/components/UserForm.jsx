f4dd062112fe218f781199bacb65102cb393f561
Rafael Toledo Souza <ch1at@outlook.com>
2026-01-30 14:31:01 -0300

    User form html and new styles

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -3,7 +3,26 @@ import React from "react";
 const UserForm = () => {
   return (
     <div>
-      <h2>User Form</h2>
+      <div className="form-control">
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Type your name"
+          required
+        />
+      </div>
+      <div className="form-control">
+        <label htmlFor="name">E-mail:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Type your e-mail"
+          required
+        />
+      </div>
     </div>
   );
 };
