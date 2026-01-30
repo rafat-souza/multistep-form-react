e1fad41c8f18ad288060aadb5f7e03a89ff22770
Rafael Toledo Souza <ch1at@outlook.com>
2026-01-30 10:46:18 -0300

    base jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Multistep</h1>
+    <div className="app">
+      <div className="header">
+        <h2>Leave your review</h2>
+        <p>
+          We are happy for your purchase, fill out the form below to rate the
+          product
+        </p>
+      </div>
+      <div className="form-container">
+        <p>steps</p>
+        <form>
+          <div className="inputs-container"></div>
+          <div className="actions">
+            <button type="button">
+              <GrFormPrevious />
+              <span>Back</span>
+            </button>
+            <button type="submit">
+              <span>Next</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
